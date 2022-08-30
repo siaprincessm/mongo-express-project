53b791cc8afad34371579ed1d1124ce37ba7b94e
siaprincessm <siaprincessm@gmail.com>
2022-08-30 15:58:18 +0000

    get

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config();
+
+
+// console.log(process.env);
 
 const mongoUtil = require('./MongoUtil');
-
+const { ObjectId } = require('mongodb');
 const app = express();
 
 app.use(express.json())
 app.use(cors());
 
-const MONGO_URI = "mongodb+srv://siaprincessm:66yqjw686lK5x6QP@cluster0.pqwqho7.mongodb.net/?retryWrites=true&w=majority";
-const DB_NAME = "dwad_e_exercises";
+const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = process.env.DB_NAME;
 
 
 async function main() {
    const db = await mongoUtil.connect(MONGO_URI, DB_NAME);
+
+//Read
 
    app.get('/', function (req, res) {
         res.json({
@@ -21,21 +27,105 @@ async function main() {
         });
    })
 
-    app.post('/reviews', async function (req,res){
-        await db.collection('reviews').insertOne({
-            "_id":"123",
-            "title":"Good Steak",
-            "food":"Sirloin Steak",
-            "Content":"perfectly cooked steak",
-            "Rating":"7"
+   app.get('/routines', async function (req,res){
+
+    let criteria = {};
+
+    if (req.query.mainGoal) {
+        criteria.mainGoal = {
+            '$regex': req.query.mainGoal,
+            'options': 'i'
+        }
+    }
+
+    if (req.query.min_durationInMinutes){
+        criteria.durationInMinutes = {
+            '$gte':parseInt(req.query.min_durationInMinutes)
+        }
+    }
+
+    if (req.query.min_durationInMinutes){
+        criteria.durationInMinutes = {
+            '$gte':parseInt(req.query.min_durationInMinutes)
+        }
+    }
+
+    // console.log("criteria=", criteria);
+
+    const routines = await db.collection('routines').find(criteria).toArray();
+    res.json(routines);
+    })
+
+// Create
+
+    app.post('/routines', async function (req,res){
+        await db.collection('routines').insertOne({
+            "mainGoal" : req.body.mainGoal,
+            "workoutType": req.body.workoutType,
+            "intensity": req.body.intensity,
+            "equipmentNeeded": req.body.equipmentNeeded,
+            "durationInMinutes": req.body.durationInMinutes,
+            "exercises":req.body.exercises
         })
         res.json({
-            'message':'ok'
+            'message':'created'
         })
 
     })
-}
 
+//Update
+
+    app.put('/routines/:routinesId', async function (req,res){
+        console.log(req.params.routinesId);
+
+        await db.collection('routines').updateOne({
+            '_id': ObjectId(req.params.reviewId)
+        },{
+            "$set":{
+                "intensity": req.body.intensity,
+                "equipmentNeeded": req.body.equipmentNeeded,
+                "durationInMinutes": req.body.durationInMinutes,
+                "exercises":req.body.exercises
+            }
+        });
+        
+        res.json({
+            'message':'put received'
+        })
+    });
+
+    //Update with comment
+
+    app.put('/routines/:routinesId/comments', async function (req,res){
+        console.log(req.params.routinesId);
+
+        await db.collection('routines').updateOne({
+            '_id': ObjectId(req.params.routinesId)
+        },{
+            "$set":{
+                'comment': req.body.comment,
+                'name': req.body.name
+            }
+        });
+        
+        res.json({
+            'message':'put received'
+        })
+    });
+
+    app.delete('/routines/:routinesId', async function (req,res){
+        console.log(req.params.routinesId);
+
+        await db.collection('routines').deleteOne({
+            '_id': ObjectId(req.params.routinesId)
+        });
+        
+        res.json({
+            'message':'deleted'
+        })
+    });
+
+}
 main();
 
 app.listen(3000, function(){
